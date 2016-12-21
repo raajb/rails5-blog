ab417c75b04e66741bc5bf4f1f4786b860328f13
raajb <raaj@metabahn.com>
2016-12-21 10:51:31 -0600

    Rails 5 basics wrap up

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161206215006) do
+ActiveRecord::Schema.define(version: 20161207015038) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "body"
   end
 
 end
