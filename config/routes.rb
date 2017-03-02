715a52352145e7f6fd62692e9939ad41ef205443
raajb <raaj@metabahn.com>
2017-03-02 11:52:14 -0600

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   resources :posts
+  resources :pages
+  # resources :pages produces the following
+  # get '/pages', to: 'pages#index'
+  # post '/pages', to: 'pages#create'
+  # get '/pages/new', to: 'pages#new', as: 'new_page'
+  # get '/pages/:id', to: 'pages#show', as: 'page'
+  # get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+  # patch '/pages/:id', to: 'pages#update'
+  # delete 'pages/:id', to: 'pages#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
